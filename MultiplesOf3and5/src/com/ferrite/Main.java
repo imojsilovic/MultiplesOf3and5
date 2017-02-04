b6ae431f91d44c84e68f74333f732ddb8e16eb19
imojsilovic <imojsilovic@gmail.com>
2017-02-04 15:19:13 +0100

    First implementation:  - Calculation method

--- MultiplesOf3and5/src/com/ferrite/Main.java
+++ MultiplesOf3and5/src/com/ferrite/Main.java
@@ -3,7 +3,17 @@ package com.ferrite;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Start...");
+		System.out.println("Calculation method.");
+
+		long beginTime = System.nanoTime();
+		long limit = 1_000_000L;
+
+		long sum = (3 * (limit / 3 * (limit / 3 + 1)) + 5 * (limit / 5 * (limit / 5 + 1))
+				- 15 * (limit / 15 * (limit / 15 + 1))) / 2;
+
+		System.out.println(String.format("Time: %d [ns]", (System.nanoTime() - beginTime)));
+		System.out.println("Sum: " + sum);
 
 	}
 
