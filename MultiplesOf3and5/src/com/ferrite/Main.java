34acdc9ba5ccd21d51c9a90c64c9dd316b31f8f0
imojsilovic <imojsilovic@gmail.com>
2017-02-04 16:14:42 +0100

    Decorations...

--- MultiplesOf3and5/src/com/ferrite/Main.java
+++ MultiplesOf3and5/src/com/ferrite/Main.java
@@ -4,32 +4,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Start...");
+		System.out.println("Long.MAX_VALUE: " + Long.MAX_VALUE);
 		System.out.println("Calculation method.");
 
 		long beginTime = System.nanoTime();
-		long limit = 1_000_000L;
+		long limit = 1_000_000_000L;
 
-		long sum = (3 * (limit / 3 * (limit / 3 + 1)) + 5 * (limit / 5 * (limit / 5 + 1))
-				- 15 * (limit / 15 * (limit / 15 + 1))) / 2;
+		long sum = (3 * (limit / 3 * (limit / 3 + 1)) - 15 * (limit / 15 * (limit / 15 + 1))
+				+ 5 * (limit / 5 * (limit / 5 + 1))) / 2;
 
 		System.out.println(String.format("Time: %d [ns]", (System.nanoTime() - beginTime)));
 		System.out.println("Sum: " + sum);
-		
+
 		System.out.println();
 		System.out.println("Iteration method.");
-		
+
 		sum = 0L;
 		beginTime = System.nanoTime();
-		
-		for(long i = 0L; i < limit; ++i){
-			if(i % 3 == 0 || i % 5 == 0){
+
+		for (long i = 0L; i < limit; ++i) {
+			if (i % 3 == 0 || i % 5 == 0) {
 				sum += i;
 			}
 		}
-		
+
 		System.out.println(String.format("Time: %d [ns]", (System.nanoTime() - beginTime)));
 		System.out.println("Sum: " + sum);
-		
+
 		System.out.println("End.");
 
 	}
